64a9988b1144e7f83f1eef0170bbf63d55d37a3b
The Rushee <rashmikafan05@gmail.com>
2025-03-07 11:22:51 +0530

    just try

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Program
+{
+    static int Operation(int number_one, int number_two, char symbol)
+    {
+        switch (symbol)
+        {
+            case '+':
+                return number_one + number_two;
+            case '-':
+                return number_one - number_two;
+            case '*':
+                return number_one * number_two;
+            case '/':
+                if (number_two != 0)
+                    return number_one / number_two;
+                else
+                {
+                    Console.WriteLine("Cannot divide by zero!");
+                    return 0;
+                }
+            default:
+                Console.WriteLine("Invalid operator!");
+                return 0;
+        }
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter first number: ");
+        int number_one = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        int number_two = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter operator (+, -, *, /): ");
+        char symbol = Convert.ToChar(Console.ReadLine());
+
+        int result = Operation(number_one, number_two, symbol);
+        Console.WriteLine("Result: " + result);
+    }
+}
